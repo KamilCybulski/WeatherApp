4531fd95cde330407155e42152e42965809d3d91
Kamyl <kamillcybulski@gmail.com>
2017-02-05 00:53:02 +0100

    Fixed geoloaction. Added fetch request for weather data.

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ function getPosition() {
 
     //function for succesfully loading geolocation data
     function geoSuccess(position) {
-        var lat = position.coords.latitude;
-        var lon = position.coords.longitude;
+        var lat = position.coords.latitude + "";
+        var lon = position.coords.longitude + "";
+        console.log("Coords: " + lat + lon);
 
-        console.log("Lat: " + lat + ", Lon: " + lon);
+        return lat + "," + lon
     }
 
     //function for errors while loading geoloaction data
@@ -21,4 +22,34 @@ function getPosition() {
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
 }
 
-window.addEventListener('DOMContentLoaded', getPosition)
+//Function for getting weather data and inserting it into DOM
+function getWeather() {
+
+    //Prepare URL for fetch()
+    var coords = getPosition();
+    var key = "e984c7d121044a32a18221132170402";
+    var url = 'http://api.apixu.com/v1/current.json?key=' + key + '&q=' + coords;
+
+    fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+
+        //Variable for weather data
+        var weather = {
+            city: data.location.name,
+            country: data.location.country,
+            tempC: data.current.temp_C,
+            tempF: data.current.temp_F,
+            sky: data.current.condition.code,
+            wind: data.current.wind_kph,
+            pressure: data.current.pressure.mb;
+        } 
+        console.log(weather);
+
+        
+
+    })
+
+}
