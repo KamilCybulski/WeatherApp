2be5c93664fe3ec477957529f6670ae763988904
Kamyl <kamillcybulski@gmail.com>
2017-02-05 00:12:25 +0100

    Added geolocation.

---
+++ main.js
@@ -0,0 +1,24 @@
+//Function for getting users coordinates
+function getPosition() {
+    if (!navigator.geolocation) {
+        var location = document.getElementsByClassName("location")
+        location.innerHTML = "Your browser does not support geolocation";
+    }
+
+    //function for succesfully loading geolocation data
+    function geoSuccess() {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+
+        console.log("Latitude: " + lat + ", Longitude: " + lon);
+    }
+
+    //function for errors while loading geoloaction data
+    function geoError() {
+        console.log("Something went wrong...")
+    }
+
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+}
+
+window.addEventListener('DOMContentLoaded', getPosition)
