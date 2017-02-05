5097ae479a5508e4e1c597780e9f27e2c23f7dea
Kamyl <kamillcybulski@gmail.com>
2017-02-05 22:38:18 +0100

    Removed placeholder text from markup.

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ function getWeather(coordinates){
         //Some variables to create url
         var key = "e984c7d121044a32a18221132170402";
         var latLon = coordinates;
-        var url = "http://api.apixu.com/v1/current.json?key=" + key + "&q=" + latLon;
+        var url = "https://api.apixu.com/v1/current.json?key=" + key + "&q=" + latLon;
 
         var weatherData = fetch(url)
                             .then(function(data){
